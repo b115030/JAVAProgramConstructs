b7f972e924c36738207283f1267e06344f29ea6f
b115030 <astrogokul@DESKTOP-UI91DL7.localdomain>
2020-08-21 13:07:36 +0530

    ADDED:Welcome Message for Employee Wage Computation Problem

---
+++ EmployeeWageComputationProblem/EmpWageComp.java
@@ -0,0 +1,8 @@
+public class EmpWageComp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("**Welcome to Employee Wage Computation Problem**");
+	}
+
+}
