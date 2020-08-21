098edac50f87626dd3eaee1f86b25f01f551cdd4
b115030 <astrogokul@DESKTOP-UI91DL7.localdomain>
2020-08-21 17:26:14 +0530

    ADDED: New branch 'Attendance', Check attendance

--- RoughWorkProgs/wage.java
+++ RoughWorkProgs/wage.java
@@ -1,8 +1,22 @@
 public class wage {
-
+   public int attendance() {
+	   
+	   int n=(int)Math.floor((Math.random()*10)%3);
+	   return n;
+   }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		wage ewc =new wage();
 		System.out.println("**Welcome to Employee Wage Computation Problem**");
+		for(int i=1;i<=20;i++) {
+			System.out.print("Attendance of Employee on "+i+"th day:");
+			if(ewc.attendance()==1) {
+				System.out.println("Present");
+			}
+			else {
+				System.out.println("Absent");
+			}
+		}
 	}
 
 }
+
