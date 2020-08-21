260b1342b28d2489668d5427b6e7855c3a81d8e5
b115030 <astrogokul@DESKTOP-UI91DL7.localdomain>
2020-08-21 17:45:03 +0530

    ADDED: New branch 'CalculateDailyWage', UPDATED: wage.java, calculates daily employeewage

--- RoughWorkProgs/wage.java
+++ RoughWorkProgs/wage.java
@@ -1,4 +1,7 @@
 public class wage {
+
+	public final int WAGE_PER_HOUR=20;
+	public final int HOURS_PER_DAY=8;
    public int attendance() {
 	   
 	   int n=(int)Math.floor((Math.random()*10)%3);
@@ -8,15 +11,13 @@ public class wage {
 		wage ewc =new wage();
 		System.out.println("**Welcome to Employee Wage Computation Problem**");
 		for(int i=1;i<=20;i++) {
-			System.out.print("Attendance of Employee on "+i+"th day:");
+			System.out.print("Wage of Employee on"+i+"th day:");
 			if(ewc.attendance()==1) {
-				System.out.println("Present");
+				System.out.println((ewc.WAGE_PER_HOUR*ewc.HOURS_PER_DAY)+"Rs");
 			}
 			else {
-				System.out.println("Absent");
+				System.out.println("0 Rs");
 			}
 		}
 	}
-
 }
-
